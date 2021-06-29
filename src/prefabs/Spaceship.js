eaf0f397e994f02afeef2aee783aae8bc6abfef9
Cameron Henritzy <camhenritzy@gmail.com>
2021-06-29 14:54:38 -0700

    Have spaceships moving. have ship reset error to solve...

--- src/prefabs/Spaceship.js
+++ src/prefabs/Spaceship.js
@@ -1,17 +1,17 @@
-//class Spaceship extends Phaser.GameObjects.Sprite {
-  //  constructor(scene, x, y, texture, frame, pointValue) {
-    //    super(scene, x, y, texture, frame);
-      //  scene.add.existing(this); // add to exisiting scene
-        //this.points = pointValue; // store pointValue
-        //this.moveSpeed = game.settings.spaceshipSpeed; // pixels per frame
-    //}
+class Spaceship extends Phaser.GameObjects.Sprite {
+    constructor(scene, x, y, texture, frame, pointValue) {
+        super(scene, x, y, texture, frame);
+        scene.add.existing(this); // add to exisiting scene
+        this.points = pointValue; // store pointValue
+        this.moveSpeed = game.settings.spaceshipSpeed; // pixels per frame
+    }
 
-    //update() {
+    update() {
         // move spaceship left
-      //  this.x -= this.moveSpeed;
+        this.x -= this.moveSpeed;
         // wrap around from left edge to right edge
-        //if (this.x <= 0 - this.width) {
-          //  this.x = game.config.width;
-        //}
-    //}
-//}
+        if (this.x <= 0 - this.width) {
+            this.x = game.config.width;
+        }
+    }
+}
